caf655a1a61a3367c5d5ad33a5ae5dc71c38c0f2
Erik Stainsby <erik.stainsby@roaringsky.ca>
2012-02-18 21:04:18 -0800

    stubbed in

---
+++ ActionDrop/ActionDrop.h
@@ -0,0 +1,13 @@
+//
+//  ActionDrop.h
+//  ActionDrop
+//
+//  Created by Erik Stainsby on 12-02-18.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import <RSTrixiePlugin/RSTrixiePlugin.h>
+
+@interface ActionDrop : RSTrixiePlugin
+
+@end
---
+++ ActionDrop/ActionDrop.m
@@ -0,0 +1,26 @@
+//
+//  ActionDrop.m
+//  ActionDrop
+//
+//  Created by Erik Stainsby on 12-02-18.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import "ActionDrop.h"
+
+@implementation ActionDrop
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+	
+    if( nil != (self = [super initWithNibName:nibNameOrNil	bundle:nibBundleOrNil]))
+    {
+		[self setName: @"Drop"];
+    }
+    return self;
+}
+
+- (BOOL) hasSelectorField {
+	return NO;
+}
+
+@end
